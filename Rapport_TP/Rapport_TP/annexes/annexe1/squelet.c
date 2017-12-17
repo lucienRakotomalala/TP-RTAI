55c7755c3d3a5589e0acfcb95bb87d603982821a
DavidTocaven <David.tocaven@gmail.com>
2017-12-17 20:46:24 +0100

    annexe 1 part 2 finie

--- Rapport_TP/Rapport_TP/annexes/annexe1/squelet.c
+++ Rapport_TP/Rapport_TP/annexes/annexe1/squelet.c
@@ -12,16 +12,10 @@ MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Squelette de programme RTAI et carte ni-6221");
 MODULE_AUTHOR("RAKOTOMALALA FARHI TOCAVEN");
 
-
-/*
- *	command line parameters
- */
-
-
+// times unit
 #define ms  1000000
-
 #define microsec 1000
-
+// min-max output signal values
 #define CARRE_HIGH 1.22
 #define CARRE_LOW 0
 
@@ -38,94 +32,83 @@ MODULE_AUTHOR("RAKOTOMALALA FARHI TOCAVEN");
 static RT_TASK Tache1_Ptr; // Pointeur pour la tache 1
 static RT_TASK IT_handler_Ptr; // Pointeur pour la tache de reprise de main
 
-
 comedi_t *cf; // la carte
 RTIME curTime; // un timer pour mesurer les dif de temps
 
 /******************* tache1 ***************************/
 void Tache1 (long int x)
 {
-	int voie = x,
-	    composant=DIO ,
-	    i=0;/*position dans deltat*/
-
-	int delta_i = 0;
-	  while (1)
-	  {  
-	  	curTime = rt_get_time();
-	  	delta_i=(int)curTime;
-	  	
+	int voie 		= x,
+	    composant 	= DIO,
+	    i			=0,		/*position dans deltat*/
+		delta_i 	= 0;
+	while (1)
+	{  
+		curTime = rt_get_time(); // read beginning time
+		delta_i=(int)curTime; // keep current time
 		comedi_dio_write(cf,composant,voie,CARRE_HIGH);
 		rt_task_wait_period();
 		comedi_dio_write(cf,composant,voie,CARRE_LOW);
 		rt_task_wait_period();
+		curTime = rt_get_time(); // read final times
+		delta_i = (int)curTime - delta_i; // calc period
 
-		curTime = rt_get_time();
-		delta_i = (int)curTime - delta_i;
-	  	
-	  	i=(i+1)%SIZETAB;
-	  	if( i > SIZETAB)
-	  	{	
-		  	if(rtf_put(deltat,&delta_i,SIZETAB)<0)
-		  	{
+		i=(i+1)%SIZETAB; // increment counter modulo SIZETAB for count the number of values
+		if( i > SIZETAB)
+		{	
+			if(rtf_put(deltat,&delta_i,SIZETAB)<0)
+			{
 				printk("probleme ecrite fifo : /dev/rtf0\n");
-
 			}
-	  	}
-		
-	  }
+		}
+	}
 }
-
 
 /******************* init ***************************/
 
 int init_module(void)
 {	
-  RTIME now ;
+	RTIME now ;
  	 	// Initialisation de la carte d'E/S
-  cf = comedi_open("/dev/comedi0");	
-  if(cf == NULL)
-  {
-    comedi_perror("Comedi fails to open");
-    return -1;
-  }
-
+	cf = comedi_open("/dev/comedi0");	
+	if(cf == NULL)
+	{
+		comedi_perror("Comedi fails to open");
+		return -1;
+	}
 	// init FIFO pour mesures
-	
 	// Configurer le device DIGITAL_INPUT pour recevoir les donnees signaux
 	// et DIGITAL_OUTPUT pour envoyer les donnees signaux
 	comedi_dio_config(cf,DIO,0,COMEDI_OUTPUT);
    	comedi_dio_config(cf,DIO,1,COMEDI_OUTPUT);
-   	
 	rt_set_oneshot_mode();
 	// Lancement du timer
 
-	now =  start_rt_timer(0); // création timer de période  50ms
+	now =  start_rt_timer(0); // creation timer de periode  50ms
 	curTime = start_rt_timer( nano2count(0));
  	now = rt_get_time();
   	// Lancement des taches
  	rt_task_init(&Tache1_Ptr, 	/* Le prt de tache */
 	 	      Tache1,	   	/* Nom de la tache */
-	 	      1, 		/* valeur du paramètre X */
-	 	      2000,		/* Taille de la pile necessaire pour la tache (memoire tempo utilisée) */
-	 	      0,		/* Priorité */
+	 	      1, 		/* valeur du parametre X */
+	 	      2000,		/* Taille de la pile necessaire pour la tache (memoire tempo utilisee) */
+	 	      0,		/* Priorite */
 	 	      0,  		/* Pas de calcul flottant */
 	 	      0); 		/* choix d'un signal ou non  */
 	 	      
 
-	rt_task_make_periodic(	&Tache1_Ptr, 	/* Pointeur vers la tâche */
-				now,		/* Instant de départ */
+	rt_task_make_periodic(	&Tache1_Ptr, 	/* Pointeur vers la tache */
+				now,		/* Instant de depart */
 				nano2count(PERIOD*microsec/2));		/* Periode */
 				
 				
 	rtf_create(deltat, SIZETAB);
-  return 0;
+	return 0;
 }
 
 /******************* cleanup ***************************/
 void cleanup_module(void)
 {
-  stop_rt_timer();
-rt_task_delete(&Tache1_Ptr);
-  
+	stop_rt_timer();
+	rt_task_delete(&Tache1_Ptr);
  }
